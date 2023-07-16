ad1463b3fd7e67b3b39f7113ba85eddd21ec47f4
Agung Saputra <agungsaputradifh@gmail.com>
2023-07-16 14:44:52 +0700

    added functions

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+$hai = "hai";
+$koneksi = mysqli_connect("localhost", "root", "", "kasir");
+function tampil($query)
+{
+    global $koneksi;
+    $result = mysqli_query($koneksi, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
