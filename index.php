7db45021ae993c67ab3d5d708fe15d8f464e9787
Agung Saputra <agungsaputradifh@gmail.com>
2023-07-16 19:56:27 +0700

    buat template daftar

--- index.php
+++ index.php
@@ -8,11 +8,22 @@ $daftars = tampil("SELECT * FROM daftar");
 
 <head>
     <title>Kasir App</title>
-    <link rel="stylesheet" href="css/output.css">
+    <link rel="stylesheet" href="./css/output.css">
+    <!-- <meta http-equiv="refresh" content="5"> -->
 </head>
 
-<body>
-    <h2 class="">Kasir App - Admin Side</h2>
+<body class="p-5 bg-[#54efc3]">
+    <h1 class="tittle">Kasir App - Admin Side</h1>
+    <div class="flex flex-wrap gap-5">
+        <div class="daftar">
+            <h1 class="daftar-nama">Mie Ayam bawang</h1>
+            <img src="./daftar/mieayambawang.jpg" alt="mieayam" class="daftar-gambar">
+            <div class="daftar-keterangan-1">
+                <h1 class="daftar-keterangan-1-1">Rp.10000</h1>
+                <h1 class="daftar-keterangan-1-1">Stock : 50</h1>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*/*.{html, js, php}"],
+  content: ["./index.php"],
   theme: {
     extend: {},
   },
