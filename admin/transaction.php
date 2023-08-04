3bd522933898c7d97fdef4a2a4cf0ec431d6ff3c
Agung Saputra <agungsaputradifh@gmail.com>
2023-08-04 23:49:20 +0700

    new desain transaction

--- admin/transaction.php
+++ admin/transaction.php
@@ -1,7 +1,12 @@
 <?php
+session_start();
+if (!isset($_SESSION["login"])) {
+    header("Location: ./login.php");
+    die;
+}
 require '../function/functions.php';
 
-$transactions;
+$transactions = read("SELECT * FROM transaction");
 
 ?>
 
@@ -15,7 +20,7 @@ $transactions;
     <link rel="stylesheet" href="../css/output.css">
 </head>
 
-<body>
+<body class="bg-slate-200 ">
     <nav class="bg-bg2 py-3 lg:py-5 text-white flex items-center fixed w-full justify-between px-3 lg:px-10">
         <a href="../index.php">
             <img src="../img/icon/back.png" alt="back" class="w-[25px] lg:w-[35px] change-color">
@@ -35,6 +40,32 @@ $transactions;
             </a>
         </div>
     </nav>
+
+    <div class="py-20">
+        <?php foreach ($transactions as $transaction) : ?>
+            <div class="bg-white w-[80%] lg:w-[35%] mx-auto px-3 py-4 rounded-lg my-5">
+                <div class="flex items-center relative">
+                    <img src="../img/item/<?= $transaction['item_image'] ?>" alt="<?= $transaction['item_name'] ?>" class="h-16">
+                    <h1 class="pl-3"><?= $transaction['item_name'] ?></h1>
+                    <h1 class="font-bold absolute right-3">Rp<?= number_format($transaction['item_price'], 0, ',', '.'); ?></h1>
+                </div>
+
+                <div class="bg-slate-200 h-[1px] w-[100%] mx-auto my-3"></div>
+
+                <div class="flex justify-between">
+                    <a href="" class="button-yellow">Bukti transfer</a>
+                    <a href="" class="button-green">Confirm</a>
+                </div>
+
+                <div class="bg-slate-200 h-[1px] w-[100%] mx-auto my-3"></div>
+
+                <h1>Status : <?= $transaction['status'] ?></h1>
+            </div>
+        <?php endforeach; ?>
+        <?php if (!$transactions) : ?>
+            <h1>Anda harus melakukan pemesanan terlebih dahulu</h1>
+        <?php endif; ?>
+    </div>
 </body>
 
 </html>
