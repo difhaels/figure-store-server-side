f1219bcfd1a4f875e6b0681e103a706469bcca42
Agung Saputra <agungsaputradifh@gmail.com>
2023-08-05 18:34:13 +0700

    ubah desain index

--- index.php
+++ index.php
@@ -18,31 +18,33 @@ $items = read("SELECT * FROM item");
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Figrue Store Server Side</title>
     <link rel="stylesheet" href="./css/output.css">
 </head>
 
 <body class="main">
-    <div class="bg-bg2 py-5 text-white flex items-center fixed w-full justify-between px-10">
+    <nav class="bg-bg2 py-3 lg:py-5 text-white flex items-center fixed w-full justify-between px-3 lg:px-10">
         <div class="flex items-end">
-            <h1 class="font-extrabold text-4xl">FIGURE STORE</h1>
-            <h1>.server side</h1>
+            <h1 class="font-extrabold text-lg lg:text-4xl">FIGURE STORE</h1>
+            <h1 class="text-[9px] lg:text-base pt-2 lg:pt-0">.server side</h1>
         </div>
         <div class="flex gap-6">
             <a href="admin/transaction.php">
-                <img src="./img/icon/pemesanan.png" class="w-[70px] lg:w-[45px] change-color">
+                <img src="./img/icon/pemesanan.png" class="w-[25px] lg:w-[35px] change-color">
             </a>
             <a href="item/create.php">
-                <img src="./img/icon/create.png" class="w-[66px] lg:w-[42px] change-color">
+                <img src="./img/icon/create.png" class="w-[25px] lg:w-[35px] change-color">
             </a>
             <a href="admin/admin.php">
-                <img src="./img/icon/admin.png" class="w-[66px] lg:w-[42px] change-color">
+                <img src="./img/icon/admin.png" class="w-[25px] lg:w-[35px] change-color">
             </a>
             <a href="admin/logout.php">
-                <img src="./img/icon/logout.png" class="w-[65px] lg:w-[41px] change-color">
+                <img src="./img/icon/logout.png" class="w-[25px] lg:w-[35px] change-color">
             </a>
         </div>
-    </div>
+    </nav>
 
     <div class="p-10 pt-40 lg:pt-32">
         <div class="flex flex-wrap gap-2 justify-center">
